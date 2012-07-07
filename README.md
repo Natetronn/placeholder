f9f87f446ecac85d82bb864f61d3f41b2926d638
Nathan Doyle <natetronn@gmail.com>
2012-07-06 21:30:26 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,160 @@
-placeholder
-===========
+# Placeholder #
+## An ExpressionEngine Image Placeholding Add-on ##
 
-Placeholder allows you to easily add placeholder images to your site. Big deal right? Well, not really but, the nice thing about Placeholder is it currently combines 4 popular placeholder image servcies into one! You want cute kittens, no problem. You want dogs to chase the cute kittens, bark bark. Just need some generic non-image looking images, sure thing. How about a B&amp;W parasailer gliding over a scenic mountain-scape? Well, I'm not sure that's available but, it might be.
+> Placeholder allows you to easily add placeholder images to your site. Big deal right? Well, not really but, the nice thing about Placeholder is it currently combines 4 popular placeholder image servcies into one! You want cute kittens, no problem. You want dogs to chase the cute kittens, bark bark. Just need some generic non-image looking images, sure thing. How about a B&W parasailer gliding over a scenic mountain-scape? Well, I'm not sure that's available but, it might be.
+
+
+## =====placehold.it====== ##
+The following params are available for placehold.it:
+
+- width (required)
+- height
+- text
+- bg_color
+- color
+- format [jpg|jpeg|gif|png]
+- extra (custom)
+
+**Example usage:**
+
+`{exp:placeholder:ph width="300" height="250" text="Nate+is+amazing!" format="jpg"}`
+
+`{exp:placeholder:ph width="300" height="250" bg_color="E8117F" color="FFF"}`
+
+**Aliases:**
+
+`{exp:placeholder:ph}`
+
+`{exp:placeholder:phit}`
+
+`{exp:placeholder:holdit}`
+
+`{exp:placeholder:placeholdit}`
+
+**Notes:**
+
+
+- *Use hex colors for the color and bg_color params.*
+- *Text and Format params can't be used with bg_color or color apparently.*
+- *If you don't add a height your image will be a squared based on your what you set the width param to.*
+- *The text param uses the + symbol for spaces.*
+
+
+## ======{placekitten}====== ##
+The following params are available for placekitten:
+
+- width (required)
+- height
+- greyscale [yes]
+- extra (custom)
+
+**Example usage:**
+
+{exp:placeholder:pk width="300" height="600" greyscale="yes"}
+
+**Aliases:**
+
+`{exp:placeholder:pk}`
+
+`{exp:placeholder:kitten}`
+
+`{exp:placeholder:placekitten}`
+
+`{exp:placeholder:i_hate_dogs}`
+
+**Notes:**
+
+*If you don't add a height your image will be a squared based on your set width.*
+
+
+## ======PlaceDog====== ##
+The following params are available for PlaceDog:
+
+- width (required)
+- height (required)
+- greyscale [yes]
+- extra (custom)
+
+**Example usage:**
+
+{exp:placeholder:pd width="300" height="600" greyscale="yes"}
+
+**Aliases:**
+
+`{exp:placeholder:pd}`
+
+`{exp:placeholder:dog}`
+
+`{exp:placeholder:placedog}`
+
+`{exp:placeholder:i_hate_cats}`
+
+## ======LoremPixel====== ##
+The following params are available for LoremPixel:
+
+- width (required)
+- height (required)
+- text
+- greyscale [yes]
+- category [abstract|animals|city|food|nightlife|fashion|people|nature|sports|technics|transport]
+- img_number [1-10]
+- extra (custom)
+
+**Example usage:**
+
+`{exp:placeholder:lp width="300" height="400" category="sports" img_number="3" text="Flying high" greyscale="yes"}`
+
+**Aliases:**
+
+`{exp:placeholder:lp}`
+
+`{exp:placeholder:pixel}`
+
+`{exp:placeholder:lorempixel}`
+
+**Notes:**
+
+*Looks like special characters don't work with this service. Use [A-Z­a-z­0-9­-]*
+
+
+## ======Other Notes====== ##
+The extra param may be used to manually add things like class, id, title, alt, rel etc. - note the single quotes
+
+**Example usage:**
+
+`{exp:placeholder:pk width="300" height="600" greyscale="yes" extra='alt="Cool pic" id="someID" class="someClass"'}	`
+
+## ======Compatibility====== ##
+
+**ExpressionEngine 2.x+**
+
+*Tested in 2.4.0*
+
+## ======Installation====== ##
+
+**For EE2**
+
+Copy *system/expressionengine/third_party/placeholder* to your *system/expressionengine/third_party* directory
+
+## ======Change Log====== ##
+
+**Jul 06, 2012: 1.0.0**
+
+*Initial Release*
+
+## ======Support====== ##
+
+
+[http://natetronn.com](http://natetronn.com)
+
+[@natetronn](http://twitter.com/natetronn)
+
+## ======Credits====== ##
+
+[http://placekitten.com](http://placekitten.com)
+
+[http://placedog.com](http://placedog.com)
+
+[http://placehold.it](http://placehold.it)
+
+[http://lorempixel.com](http://lorempixel.com)
---
+++ system/expressionengine/third_party/placeholder/pi.placeholder.php
@@ -0,0 +1,475 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Placeholder
+ *
+ * @package			Placeholder
+ * @version			1.0.0
+ * @author			Nathan Doyle <@natetronn>
+ * @copyright		Copyright (c) 2012 Cosmos Web Works, LLC
+ * @license			MIT  http://opensource.org/licenses/mit-license.php
+ * @link				http://github.com/Natetronn/placeholder
+ */
+ 
+/**
+ * Placeholder - Plugin
+ */
+
+$plugin_info = array(
+	'pi_name'				=> 'Placeholder',
+	'pi_version'		=> '1.0.0',
+	'pi_author'			=> 'Nathan Doyle',
+	'pi_author_url'	=> 'http://natetronn.com',
+	'pi_description'=> 'An ExpressionEngine Image Placeholding Add-on',
+	'pi_usage'			=> Placeholder::usage()
+);
+
+
+class Placeholder {
+
+	public $return_data;
+    
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+		$this->EE =& get_instance();
+	}
+	
+	public function ph()
+	{
+	
+		$width 			= $this->EE->TMPL->fetch_param('width');
+		$height 		= $this->EE->TMPL->fetch_param('height');
+		$bg_color 	= $this->EE->TMPL->fetch_param('bg_color');
+		$color 			= $this->EE->TMPL->fetch_param('color');
+		$format 		= $this->EE->TMPL->fetch_param('format');
+		$text 			= $this->EE->TMPL->fetch_param('text');
+		$extra 			= $this->EE->TMPL->fetch_param('extra');
+		
+		// let's help them out if they add the pixels for some unknown reason
+		$width 			= str_replace('.px','',$width);
+		$height 		= str_replace('.px','',$height);
+		
+		// let's help them out by removing the # character which we won't be needing
+		$color = str_replace('#','',$color);
+		$bg_color = str_replace('#','',$bg_color);
+		
+		// we only need one period so, if they add one let's help them out and remove it
+		$format = str_replace('.','',$format);
+
+		$src = "http://placehold.it";
+		
+		$slash = "/";
+
+		if ($width == FALSE) 
+		{
+			return 'You forgot to set the width paramater, please set it.';
+		}		
+		
+		if ($height != FALSE)
+		{
+			$size = $slash.$width."x".$height;
+		}
+		else
+		{
+			$size = $slash.$width;
+		}
+				
+		if ($bg_color != FALSE)
+		{
+			$bg_color = $slash.$bg_color;
+		}
+		
+		if ($color != FALSE)
+		{
+			$color = $slash.$color;
+		}
+		
+		if ($bg_color == FALSE && $color != FALSE)
+		{
+			$bg_color = $slash."CCC";
+		}
+		
+		if ($text != FALSE)
+		{
+			$text = "&text=".$text;
+		}
+		
+		if ($format != FALSE)
+		{
+			$format = ".".$format;
+		}
+		
+		if ($color != FALSE OR $bg_color != FALSE)
+		{
+			if ($extra != FALSE)
+			{
+				$placeholder = '<img '.$extra.' src="'.$src.$size.$bg_color.$color.'" />';
+			}
+			else
+			{
+				$placeholder = '<img src="'.$src.$size.$bg_color.$color.'" />';
+			}
+		}
+		else
+		{
+			if ($extra != FALSE)
+			{
+				$placeholder = '<img '.$extra.' src="'.$src.$size.$format.$text.'" />';
+			}
+			else
+			{
+				$placeholder = '<img src="'.$src.$size.$format.$text.'" />';
+			}
+		}
+		
+		return $placeholder;
+	}
+	
+	// Switching things up a bit for the {placekitten} service
+	public function pk()
+	{
+	
+		$width 			= $this->EE->TMPL->fetch_param('width');
+		$height 		= $this->EE->TMPL->fetch_param('height');		
+		$greyscale	=	$this->EE->TMPL->fetch_param('greyscale');
+		$extra 			= $this->EE->TMPL->fetch_param('extra');
+		
+		// let's help them out if they add the pixels for some unknown reason
+		$width 			= str_replace('.px','',$width);
+		$height 		= str_replace('.px','',$height);
+		
+		$slash = "/";
+		
+		if ($width == FALSE) 
+		{
+			return 'You forgot to set the width paramater, please set it.';
+		}	
+	
+		if (isset($greyscale) && $greyscale == "yes")
+		{
+			$src = "http://placekitten.com/g/";
+		}
+		else
+		{
+			$src = "http://placekitten.com/";
+		}
+		
+		if (isset($height))
+		{
+			$size = $width."/".$height;
+		}
+
+		if ($extra != FALSE)
+		{
+			$placeholder = '<img '.$extra.' src="'.$src.$size.'" />';
+		}
+		else
+		{
+			$placeholder = '<img src="'.$src.$size.'" />';
+		}
+		return $placeholder;
+
+	}
+	
+		// Switching things up a bit for the {placekitten} service
+	public function pd()
+	{
+	
+		$width 			= $this->EE->TMPL->fetch_param('width');
+		$height 		= $this->EE->TMPL->fetch_param('height');		
+		$greyscale	=	$this->EE->TMPL->fetch_param('greyscale');
+		$extra 			= $this->EE->TMPL->fetch_param('extra');
+		
+		// let's help them out if they add the pixels for some unknown reason
+		$width 			= str_replace('.px','',$width);
+		$height 		= str_replace('.px','',$height);
+		
+		$slash = "/";
+		
+		if ($width == FALSE) 
+		{
+			return 'You forgot to set the width paramater, please set it.';
+		}
+		
+		if ($height == FALSE) 
+		{
+			return 'You forgot to set the height paramater, please set it.';
+		}
+		
+	
+		if (isset($greyscale) && $greyscale == "yes")
+		{
+			$src = "http://placedog.com/g/";
+		}
+		else
+		{
+			$src = "http://placedog.com/";
+		}
+		
+		if (isset($height))
+		{
+			$size = $width."/".$height;
+		}
+
+		if ($extra != FALSE)
+		{
+			$placeholder = '<img '.$extra.' src="'.$src.$size.'" />';
+		}
+		else
+		{
+			$placeholder = '<img src="'.$src.$size.'" />';
+		}
+		return $placeholder;
+
+	}
+	
+	// Switching things up a bit for the lorempixel service
+	public function lp()
+	{
+	
+		$width 			= $this->EE->TMPL->fetch_param('width');
+		$height 		= $this->EE->TMPL->fetch_param('height');
+		$text 			= $this->EE->TMPL->fetch_param('text');
+		$greyscale	=	$this->EE->TMPL->fetch_param('greyscale');
+		$category		=	$this->EE->TMPL->fetch_param('category');
+		$img_number	=	$this->EE->TMPL->fetch_param('img_number');
+		$extra 			= $this->EE->TMPL->fetch_param('extra');
+		
+		// let's help them out if they add the pixels for some unknown reason
+		$width 			= str_replace('.px','',$width);
+		$height 		= str_replace('.px','',$height);
+		
+		$slash = "/";
+	
+		if ($height  == FALSE) 
+		{
+			return 'You forgot to set the height paramater, please set it.';
+		}	
+	
+		if (isset($greyscale) && $greyscale == "yes")
+		{
+			$src = "http://lorempixel.com/g/";
+		}
+		else
+		{
+			$src = "http://lorempixel.com/";
+		}
+		
+		if ($category != FALSE)
+		{
+			$category = $slash.$category;
+		}
+		
+		if ($img_number != FALSE)
+		{
+			$img_number = $slash.$img_number;
+		}
+		
+		if ($text != FALSE)
+		{
+			$text = $slash.$text;
+		}
+		
+		if ($extra != FALSE)
+		{
+		$placeholder = '<img '.$extra.' src="'.$src.$width.$slash.$height.$category.$img_number.$text.'" />';
+		}
+		else
+		{
+			$placeholder = '<img src="'.$src.$width.$slash.$height.$category.$img_number.$text.'" />';
+		}
+		
+		return $placeholder;
+	}
+
+	// lets alias our functions		
+	public function placeholdit()
+	{
+		return $this->ph();
+	}
+	
+	public function holdit()
+	{
+		return $this->ph();
+	}
+	
+	public function phit()
+	{
+		return $this->ph();
+	}
+	
+	public function placedog()
+	{
+		return $this->pd();
+	}
+	
+	public function dog()
+	{
+		return $this->pd();
+	}
+	
+	public function placekitten()
+	{
+		return $this->pk();
+	}
+	
+	public function kitten()
+	{
+		return $this->pk();
+	}
+	
+	public function lorempixel()
+	{
+		return $this->lp();
+	}
+	
+	public function pixel()
+	{
+		return $this->lp();
+	}
+	
+	public function lorem()
+	{
+		return $this->lp();
+	}
+	
+	public function i_hate_dogs()
+	{
+		return "Do you really hate dogs?";
+	}
+	
+	public function i_hate_cats()
+	{
+		return "Do you really hate cats?";
+	}
+	
+	
+	/**
+	 * Plugin Usage
+	 */
+	public static function usage()
+	{
+		ob_start();
+?>
+	
+	=====PLACEHOLD.IT======
+	The following params are available for placehold.it:
+	
+	width (required)
+	height
+	text
+	bg_color
+	color
+	format [jpg|jpeg|gif|png]
+	extra (custom)
+	
+	Example usage:
+	
+	{exp:placeholder:ph width="300" height="250" text="Nate+is+amazing!" format="jpg"}
+	
+	{exp:placeholder:ph width="300" height="250" bg_color="E8117F" color="FFF"}
+	
+	Aliases:
+	{exp:placeholder:ph}
+	{exp:placeholder:phit}
+	{exp:placeholder:holdit}
+	{exp:placeholder:placeholdit}
+	
+	Notes:
+	Use hex colors for the color and bg_color params.
+	Text and Format params can't be used with bg_color or color apparently.
+	If you don't add a height your image will be a squared based on your what you set the width param to.
+	The text param uses the + symbol for spaces.
+	
+	
+	======{placekitten}======
+	The following params are available for placekitten:
+
+	width (required)
+	height
+	greyscale [yes]
+	extra (custom)
+	
+	Example usage:
+	
+	{exp:placeholder:pk width="300" height="600" greyscale="yes"}
+	
+	Aliases:
+	{exp:placeholder:pk}
+	{exp:placeholder:kitten}
+	{exp:placeholder:placekitten}
+	{exp:placeholder:i_hate_dogs}
+	
+	Notes:
+	If you don't add a height your image will be a squared based on your set width.
+	
+	
+	======PlaceDog======
+	The following params are available for PlaceDog:
+
+	width (required)
+	height (required)
+	greyscale [yes]
+	extra (custom)
+	
+	Example usage:
+	
+	{exp:placeholder:pd width="300" height="600" greyscale="yes"}
+
+	Aliases:
+	{exp:placeholder:pd}
+	{exp:placeholder:dog}
+	{exp:placeholder:placedog}
+	{exp:placeholder:i_hate_cats}
+	
+	======LoremPixel======
+	The following params are available for LoremPixel:
+	
+	width (required)
+	height (required)
+	text
+	greyscale [yes]
+	category [abstract|animals|city|food|nightlife|fashion|people|nature|sports|technics|transport]
+	img_number [1-10]
+	extra (custom)
+	
+	Example usage:
+	
+	{exp:placeholder:lp width="300" height="400" category="sports" img_number="3" text="Flying high" greyscale="yes"}
+	
+	Aliases:
+	{exp:placeholder:lp}
+	{exp:placeholder:pixel}
+	{exp:placeholder:lorempixel}
+	
+	Notes:
+
+	Looks like special characters don't work with this service. Use [A-Za-z0-9-]
+	
+	
+	======Other Notes======
+	The extra param may be used for manually adding things like class, id, title, alt, rel etc. - note the single quotes
+	
+	Example usage:
+	
+	{exp:placeholder:pk width="300" height="600" greyscale="yes" extra='alt="Cool pic" id="someID" class="someClass"'}	
+	
+	======Credits======
+	http://placekitten.com
+	http://placedog.com
+	http://placehold.it
+	http://lorempixel.com
+
+<?php
+		$buffer = ob_get_contents();
+		ob_end_clean();
+		return $buffer;
+	}
+}
+
+
+/* End of file pi.placeholder.php */
+/* Location: /system/expressionengine/third_party/placeholder/pi.placeholder.php */
